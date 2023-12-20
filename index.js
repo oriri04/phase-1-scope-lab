beff0afeb9f1a2ab0a318e4322074cd757ff049d
oriri04 <152679239+oriri04@users.noreply.github.com>
2023-12-20 18:31:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+  upperCaseCustomerName();
+  console.log(customerName); 
+
+
+var customerName = 'bob';
+console.log(customerName);
+
+
+var bestCustomer = 'not bob';
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+console.log(bestCustomer); 
+overwriteBestCustomer();
+console.log(bestCustomer); 
+
+var bestCustomer = 'maybe bob'
+function setBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+  setBestCustomer();
+  console.log(bestCustomer); 
+  
+
+const leastFavoriteCustomer = 'Mark';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Bob';
+}
+console.log(leastFavoriteCustomer); 
+
+
+
+
+
+
